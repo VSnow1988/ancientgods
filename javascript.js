71cc4cbdab6c8ef9dbd1158b01865a65e433984b
Vincent Snow <vincentsnow@gmx.com>
2017-11-23 18:16:56 -0800

    All finished!

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function openEgyptian(){
+	window.open('egyptian.html');
+}
+
+function openNorse(){
+	window.open('norse.html');
+}
+
+function openCeltic(){
+	window.open('celtic.html');
+}
+
+function openShinto(){
+	window.open('shinto.html');
+}
+
